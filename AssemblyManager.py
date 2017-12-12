4c1c5f37d539b0e9c6e1950b2cf80878948a6342
eric fournier <eric.fournier@inspq.qc.ca>
2017-12-11 22:12:00 -0500

    Implement AssembStat class

--- AssemblyManager.py
+++ AssemblyManager.py
@@ -41,36 +41,24 @@ parser.add_argument('--cov',type=int,metavar='[Required : coverage threshold for
 args=parser.parse_args()
 
 
-class Filter():
+class AssemblyCommon(object):
     """
-    Class for Spades assemblies filtration
+    Base class for class Filter and class AssembStat
     """
 
     def __init__(self):
-
-        #Path to yaml file with specimen id list
+        # Path to yaml file with specimen id list
         self.yaml_file_path = args.yaml[0]
 
-        #Base directory with assembly fasta file
+        # Base directory with assembly fasta file
         self.assembly_dir = args.assembdir[0]
+
+        # Contig Length and coverage threshold
+        self.length_thresh = args.length[0]
+        self.cov_thresh = args.cov[0]
 
         if (not self.assembly_dir.endswith('/')):
             self.assembly_dir = self.assembly_dir + '/'
-
-        #Output directory for filtrated assembly files
-        self.out = self.assembly_dir + r"Filtred/"
-
-        #Create the output directory
-        try:
-            os.mkdir(self.out)
-        except Exception as e:
-            print "In __init__"
-            print e
-            exit(0)
-
-        #Contig Length and coverage threshold
-        self.length_thresh = args.length[0]
-        self.cov_thresh = args.cov[0]
 
     def ParseYamlFile(self):
         """
@@ -87,6 +75,28 @@ class Filter():
         #Specimen list
         self.SpecList = yaml.load(self.yam_file)[1:self.nb_spec + 1]
 
+
+class Filter(AssemblyCommon):
+    """
+    Class for Spades assemblies filtration
+    """
+
+    def __init__(self):
+
+        AssemblyCommon.__init__(self)
+
+        #Output directory for filtrated assembly files
+        self.out = self.assembly_dir + r"Filtred/"
+
+        #Create the output directory
+        try:
+            os.mkdir(self.out)
+        except Exception as e:
+            print "In __init__"
+            print e
+            exit(0)
+
+
     def DoFiltration(self,specimen):
         """
         Perform filtration of a Spades assembly files
@@ -98,16 +108,18 @@ class Filter():
         rec_to_keep = []
 
         try:
-
+            #For each record in the assembly file
             for my_rec in SeqIO.parse("{0}{1}{2}".format(self.assembly_dir,specimen,'.fasta'), "fasta"):
 
                 id_parse = re.search(r'NODE_\d+_length_(\d+)_cov_(\S+)_ID_',my_rec.id)
                 contig_length = id_parse.group(1)
                 contig_cov = id_parse.group(2)
 
+                #Keep the record only if his length and coverage are higher than threshold
                 if((int(contig_length) >= self.length_thresh)  and (float(contig_cov) >= self.cov_thresh)):
                     rec_to_keep.append(my_rec)
 
+            #Save the new filtered assembly
             SeqIO.write(rec_to_keep,self.out + specimen + "_Over_" + str(self.length_thresh) + "pb_" + str(self.cov_thresh) + "X"  + '.fasta','fasta')
 
         except Exception as e:
@@ -115,11 +127,66 @@ class Filter():
             print (e)
 
 
-class AssembStat():
+class AssembStat(AssemblyCommon):
 
     def __init__(self):
-        pass
-        #TODO to complete 
+
+        AssemblyCommon.__init__(self)
+
+        # Output directory for statistics files
+        self.out = self.assembly_dir + r"Stat/"
+
+        # Create the output directory
+        try:
+            os.mkdir(self.out)
+        except Exception as e:
+            print "In __init__"
+            print e
+            exit(0)
+
+
+    def CompileLengthAndCov(self,specimen):
+
+        mean_gc = lambda gc,length : round(gc * 100.0 / length, 0)
+        mean_weighted_coverage =  lambda covlenList,length : round(sum([float(x)/float(length) for x in covlenList]),0)
+
+        stat_file = self.out + "AssembStat_" + specimen + ".txt"
+
+        total_length = 0
+        total_gc = 0
+        covlen_list = []
+
+
+        with open(stat_file, 'w') as writef:
+
+            writef.write("NODE\tCoverage\tLength\n")
+
+            try:
+
+                for my_rec in SeqIO.parse("{0}{1}{2}".format(self.assembly_dir, specimen, '.fasta'), "fasta"):
+                    id_parse = re.search(r'NODE_(\d+)_length_(\d+)_cov_(\S+)_ID_', my_rec.id)
+                    node = id_parse.group(1)
+                    contig_length = id_parse.group(2)
+                    contig_cov = id_parse.group(3)
+
+                    if(int(contig_length) >= self.length_thresh and  int(re.search(r'(\d+)\.?',contig_cov).group(1)) >= self.cov_thresh):
+                        writef.write(node + "\t" + contig_cov + "\t" + contig_length + "\n")
+
+                        total_length += int(contig_length)
+                        total_gc += sum(my_rec.seq.count(x) for x in ['G', 'C', 'g', 'c', 'S', 's'])
+                        covlen_list.append(int(re.search(r'(\d+)\.?',contig_cov).group(1))*int(contig_length))
+
+                writef.write("\n")
+
+                writef.write("Total length\tPercent GC\tMean coverage\n")
+                writef.write(str(total_length) + '\t' + str(mean_gc(total_gc,total_length)) + '\t' + str(mean_weighted_coverage(covlen_list,total_length)))
+
+
+            except Exception as e:
+                print "In CompileLengthAndCov"
+                print (e)
+
+        writef.close()
 
 
 
@@ -129,4 +196,6 @@ if (args.do[0] == 'filter'):
     map(my_filter.DoFiltration,my_filter.SpecList)
 else:
     my_stat = AssembStat()
+    my_stat.ParseYamlFile()
+    map(my_stat.CompileLengthAndCov, my_stat.SpecList)
 
